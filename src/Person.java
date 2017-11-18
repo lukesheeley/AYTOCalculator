00e963c19d16f7b9c60e28d563050fa220880553
Luke Sheeley <lukesheeley@gmail.com>
2017-11-17 21:04:49 -0500

    forgot to save Person

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,40 @@
 
 public class Person {
-
+	/**This class represents a person on the show.
+	 * Each person has a name and a number.  The name is for readability and easy reference.
+	 * The number for the men references which order they appear in each match-up ceremony listing.
+	 * The number for both men and women will be used in calculations.*/
+	
+	//This is the name of a person.
+	private String name;
+	
+	//This is the person's number.
+	private int pid;
+	
+	/**Getters and setters for a person's name and pid.  Name and pid should be set
+	 * by constructor and never changed, but setters are included regardless.*/
+	//This is a getter method for a person's name.
+	public String getName() {
+		return name;
+	}
+	
+	//This is a setter method for a person's name.
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	//This is a getter method for a person's number.
+	public int getPid() {
+		return pid;
+	}
+	
+	//This is a setter method for a person's pid.
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	
+	//This is a constructor for the class.  It sets name and pid for a new person.
+	public Person(String name, int pid) {
+		this.name = name;
+	}
 }
