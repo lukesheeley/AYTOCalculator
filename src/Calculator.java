7956ae5af3acdb077ab38e0e91277784b9186a82
Luke Sheeley <lukesheeley@gmail.com>
2017-11-22 14:41:59 -0500

    Secondary functionality added to genPerfCount.  Now tracks what fraction of possible full match pairings each couple appears in.  I put in all data currently available for season 6.  I compared the numbers and fractions that my calculator produced to numbers provided on a blog for the show which appears to use a calculator similar to my own, and they matched up.

--- src/Calculator.java
+++ src/Calculator.java
@@ -30,6 +30,14 @@ public class Calculator {
 	//This is a list of factorials from 1 to n-1, used to generate the full match pairings.
 	private ArrayList<Integer> facts;
 	
+	//This is essentially a table of counts that will keep track of how many full match
+	//pairings that pass all data each couple is present in.  Each element is a guy pid and
+	//each element of those elements is a girl pid.  For example, if a truth booth
+	//says guy 4 and girl 7 are a perfect match, then matchFrac.get(4).get(7) should equal
+	//perfCount after running genPerfCount.  If a truth booth say guy 3 and girl 0 are a no
+	//match, then matchFrac.get(3).get(0) should be 0.
+	public ArrayList<ArrayList<Double>> matchFrac;
+	
 	public Calculator(int n, ArrayList<TruthBooth> truthBooths, ArrayList<MatchCer> matchCers) {
 		this.n = n;
 		perfCount = 0;
@@ -37,6 +45,15 @@ public class Calculator {
 		this.matchCers = matchCers;
 		genCount = 0;
 		facts = genFacts(n);
+		matchFrac = new ArrayList<ArrayList<Double>>();
+		ArrayList<Double> temp;
+		for (int i = 0; i < n; i++) {
+			temp = new ArrayList<Double>(n);
+			for (int j = 0; j < n; j++) {
+				temp.add(0.0);
+			}
+			matchFrac.add(temp);
+		}
 	}
 	
 	//This is a helper function that calculates the factorial of a given integer.
@@ -184,6 +201,16 @@ public class Calculator {
 			curFMP = genCurFMP(genCount);
 			if (checkTBAll(curFMP, truthBooths) && checkMCAll(curFMP, matchCers)) {
 				perfCount++;
+				for (int i = 0; i < n; i++) {
+					Double temp = matchFrac.get(i).get(curFMP.get(i));
+					matchFrac.get(i).set(curFMP.get(i), temp + 1); 
+				}
+			}
+		}
+		for (int j = 0; j < n; j++) {
+			for(int k = 0; k < n; k++) {
+				Double num = matchFrac.get(j).get(k);
+				matchFrac.get(j).set(k, num/perfCount);
 			}
 		}
 	}
@@ -206,28 +233,245 @@ public class Calculator {
 		Person guy1 = new Person("Anthony", 1);
 		Person guy2 = new Person("Malcolm", 2);
 		Person guy3 = new Person("Keith", 3);
+		Person guy4 = new Person("Joe", 4);
+		Person guy5 = new Person("Michael", 5);
+		Person guy6 = new Person("E$", 6);
+		Person guy7 = new Person("Clinton", 7);
+		Person guy8 = new Person("Shad", 8);
+		Person guy9 = new Person("Tyler", 9);
+		Person guy10 = new Person("Dmitry", 10);
 		Person girl0 = new Person("Alivia", 0);
 		Person girl1 = new Person("Geles", 1);
 		Person girl2 = new Person("Nurys", 2);
 		Person girl3 = new Person("Alexis", 3);
-		Match match0 = new Match(guy0, girl0);
+		Person girl4 = new Person("Zoe", 4);
+		Person girl5 = new Person("Keyana", 5);
+		Person girl6 = new Person("Jada", 6);
+		Person girl7 = new Person("Uche", 7);
+		Person girl8 = new Person("Audrey", 8);
+		Person girl9 = new Person("Nicole", 9);
+		Person girl10 = new Person("Diandra", 10);
 		Match match1 = new Match(guy1, girl1);
 		Match match2 = new Match(guy2, girl2);
 		Match match3 = new Match(guy3, girl3);
-		TruthBooth tb0 = new TruthBooth(match0, true);
+		Match match4 = new Match(guy3, girl0);
+		Match match5 = new Match(guy6, girl5);
+		Match match6 = new Match(guy7, girl7);
+		Match match7 = new Match(guy10, girl9);
 		TruthBooth tb1 = new TruthBooth(match1, false);
 		TruthBooth tb2 = new TruthBooth(match2, false);
 		TruthBooth tb3 = new TruthBooth(match3, false);
+		TruthBooth tb4 = new TruthBooth(match4, false);
+		TruthBooth tb5 = new TruthBooth(match5, false);
+		TruthBooth tb6 = new TruthBooth(match6, false);
+		TruthBooth tb7 = new TruthBooth(match7, false);
 		//System.out.println(calculatorTest.genCurFMP(0));
 		//System.out.println(calculatorTest.checkTB(calculatorTest.genCurFMP(0), tb0));
 		//System.out.println(calculatorTest.checkTB(calculatorTest.genCurFMP(0), tb1));
 		//System.out.println(calculatorTest.checkTB(calculatorTest.genCurFMP(0), tb2));
 		//System.out.println(calculatorTest.checkTB(calculatorTest.genCurFMP(0), tb3));
 		ArrayList<TruthBooth> tbs = new ArrayList<TruthBooth>();
-		tbs.add(tb0);
 		tbs.add(tb1);
 		tbs.add(tb2);
 		tbs.add(tb3);
+		tbs.add(tb4);
+		tbs.add(tb5);
+		tbs.add(tb6);
+		tbs.add(tb7);
+		
+		//Week 1 Matchup Ceremony
+		Match mu10 = new Match(guy0, girl0);
+		Match mu11 = new Match(guy1, girl1);
+		Match mu12 = new Match(guy2, girl2);
+		Match mu13 = new Match(guy3, girl3);
+		Match mu14 = new Match(guy4, girl4);
+		Match mu15 = new Match(guy5, girl5);
+		Match mu16 = new Match(guy6, girl6);
+		Match mu17 = new Match(guy7, girl7);
+		Match mu18 = new Match(guy8, girl8);
+		Match mu19 = new Match(guy9, girl9);
+		Match mu110 = new Match(guy10, girl10);
+		ArrayList<Match> matchlist1 = new ArrayList<Match>();
+		matchlist1.add(mu10);
+		matchlist1.add(mu11);
+		matchlist1.add(mu12);
+		matchlist1.add(mu13);
+		matchlist1.add(mu14);
+		matchlist1.add(mu15);
+		matchlist1.add(mu16);
+		matchlist1.add(mu17);
+		matchlist1.add(mu18);
+		matchlist1.add(mu19);
+		matchlist1.add(mu110);
+		MatchCer muc1 = new MatchCer(matchlist1, 3);
+		
+		//Week 2 Matchup Ceremony
+		Match mu20 = new Match(guy3, girl3);
+		Match mu21 = new Match(guy2, girl2);
+		Match mu22 = new Match(guy9, girl4);
+		Match mu23 = new Match(guy5, girl5);
+		Match mu24 = new Match(guy6, girl6);
+		Match mu25 = new Match(guy7, girl7);
+		Match mu26 = new Match(guy10, girl9);
+		Match mu27 = new Match(guy1, girl10);
+		Match mu28 = new Match(guy4, girl8);
+		Match mu29 = new Match(guy8, girl1);
+		Match mu210 = new Match(guy0, girl0);
+		ArrayList<Match> matchlist2 = new ArrayList<Match>();
+		matchlist2.add(mu20);
+		matchlist2.add(mu21);
+		matchlist2.add(mu22);
+		matchlist2.add(mu23);
+		matchlist2.add(mu24);
+		matchlist2.add(mu25);
+		matchlist2.add(mu26);
+		matchlist2.add(mu27);
+		matchlist2.add(mu28);
+		matchlist2.add(mu29);
+		matchlist2.add(mu210);
+		MatchCer muc2 = new MatchCer(matchlist2, 1);
+		
+		//Week 3 Matchup Ceremony
+		Match mu30 = new Match(guy2, girl1);
+		Match mu31 = new Match(guy5, girl8);
+		Match mu32 = new Match(guy4, girl4);
+		Match mu33 = new Match(guy0, girl0);
+		Match mu34 = new Match(guy9, girl9);
+		Match mu35 = new Match(guy10, girl2);
+		Match mu36 = new Match(guy8, girl5);
+		Match mu37 = new Match(guy6, girl3);
+		Match mu38 = new Match(guy7, girl7);
+		Match mu39 = new Match(guy1, girl6);
+		Match mu310 = new Match(guy3, girl10);
+		ArrayList<Match> matchlist3 = new ArrayList<Match>();
+		matchlist3.add(mu30);
+		matchlist3.add(mu31);
+		matchlist3.add(mu32);
+		matchlist3.add(mu33);
+		matchlist3.add(mu34);
+		matchlist3.add(mu35);
+		matchlist3.add(mu36);
+		matchlist3.add(mu37);
+		matchlist3.add(mu38);
+		matchlist3.add(mu39);
+		matchlist3.add(mu310);
+		MatchCer muc3 = new MatchCer(matchlist3, 2);
+				
+		//Week 4 Matchup Ceremony
+		Match mu40 = new Match(guy10, girl3);
+		Match mu41 = new Match(guy0, girl10);
+		Match mu42 = new Match(guy4, girl4);
+		Match mu43 = new Match(guy7, girl7);
+		Match mu44 = new Match(guy8, girl8);
+		Match mu45 = new Match(guy1, girl5);
+		Match mu46 = new Match(guy6, girl9);
+		Match mu47 = new Match(guy5, girl1);
+		Match mu48 = new Match(guy3, girl2);
+		Match mu49 = new Match(guy2, girl0);
+		Match mu410 = new Match(guy9, girl6);
+		ArrayList<Match> matchlist4 = new ArrayList<Match>();
+		matchlist4.add(mu40);
+		matchlist4.add(mu41);
+		matchlist4.add(mu42);
+		matchlist4.add(mu43);
+		matchlist4.add(mu44);
+		matchlist4.add(mu45);
+		matchlist4.add(mu46);
+		matchlist4.add(mu47);
+		matchlist4.add(mu48);
+		matchlist4.add(mu49);
+		matchlist4.add(mu410);
+		MatchCer muc4 = new MatchCer(matchlist4, 3);
+		
+		//Week 5 Matchup Ceremony
+		Match mu50 = new Match(guy1, girl9);
+		Match mu51 = new Match(guy2, girl10);
+		Match mu52 = new Match(guy0, girl0);
+		Match mu53 = new Match(guy4, girl4);
+		Match mu54 = new Match(guy8, girl8);
+		Match mu55 = new Match(guy9, girl5);
+		Match mu56 = new Match(guy6, girl1);
+		Match mu57 = new Match(guy3, girl3);
+		Match mu58 = new Match(guy7, girl6);
+		Match mu59 = new Match(guy10, girl7);
+		Match mu510 = new Match(guy5, girl2);
+		ArrayList<Match> matchlist5 = new ArrayList<Match>();
+		matchlist5.add(mu50);
+		matchlist5.add(mu51);
+		matchlist5.add(mu52);
+		matchlist5.add(mu53);
+		matchlist5.add(mu54);
+		matchlist5.add(mu55);
+		matchlist5.add(mu56);
+		matchlist5.add(mu57);
+		matchlist5.add(mu58);
+		matchlist5.add(mu59);
+		matchlist5.add(mu510);
+		MatchCer muc5 = new MatchCer(matchlist5, 1);
+		
+		//Week 6 Matchup Ceremony
+		Match mu60 = new Match(guy1, girl5);
+		Match mu61 = new Match(guy4, girl3);
+		Match mu62 = new Match(guy5, girl7);
+		Match mu63 = new Match(guy7, girl1);
+		Match mu64 = new Match(guy3, girl4);
+		Match mu65 = new Match(guy8, girl8);
+		Match mu66 = new Match(guy0, girl2);
+		Match mu67 = new Match(guy10, girl10);
+		Match mu68 = new Match(guy2, girl0);
+		Match mu69 = new Match(guy9, girl9);
+		Match mu610 = new Match(guy6, girl6);
+		ArrayList<Match> matchlist6 = new ArrayList<Match>();
+		matchlist6.add(mu60);
+		matchlist6.add(mu61);
+		matchlist6.add(mu62);
+		matchlist6.add(mu63);
+		matchlist6.add(mu64);
+		matchlist6.add(mu65);
+		matchlist6.add(mu66);
+		matchlist6.add(mu67);
+		matchlist6.add(mu68);
+		matchlist6.add(mu69);
+		matchlist6.add(mu610);
+		MatchCer muc6 = new MatchCer(matchlist6, 4);
+		
+		//Week 7 Matchup Ceremony
+		Match mu70 = new Match(guy0, girl2);
+		Match mu71 = new Match(guy1, girl5);
+		Match mu72 = new Match(guy5, girl8);
+		Match mu73 = new Match(guy8, girl0);
+		Match mu74 = new Match(guy3, girl6);
+		Match mu75 = new Match(guy10, girl10);
+		Match mu76 = new Match(guy4, girl7);
+		Match mu77 = new Match(guy9, girl9);
+		Match mu78 = new Match(guy6, girl4);
+		Match mu79 = new Match(guy7, girl1);
+		Match mu710 = new Match(guy2, girl3);
+		ArrayList<Match> matchlist7 = new ArrayList<Match>();
+		matchlist7.add(mu70);
+		matchlist7.add(mu71);
+		matchlist7.add(mu72);
+		matchlist7.add(mu73);
+		matchlist7.add(mu74);
+		matchlist7.add(mu75);
+		matchlist7.add(mu76);
+		matchlist7.add(mu77);
+		matchlist7.add(mu78);
+		matchlist7.add(mu79);
+		matchlist7.add(mu710);
+		MatchCer muc7 = new MatchCer(matchlist7, 5);
+		
+		
+		
+		
+		ArrayList<MatchCer> mucs = new ArrayList<MatchCer>();
+		mucs.add(muc1);
+		mucs.add(muc2);
+		mucs.add(muc3);
+		mucs.add(muc4);
+		mucs.add(muc5);
+		mucs.add(muc6);
+		mucs.add(muc7);
 		//System.out.println(calculatorTest.checkTBAll(calculatorTest.genCurFMP(0), tbs));
 		//int i = 0;
 		//while(!calculatorTest.checkTBAll(calculatorTest.genCurFMP(i), tbs)) {
@@ -236,9 +480,11 @@ public class Calculator {
 		//System.out.println(i);
 		//System.out.println(calculatorTest.genCurFMP(i));
 		//System.out.println(calculatorTest.genCurFMP(367919));
-		Calculator calculatorTest = new Calculator(11, tbs, matchCersTest); 
+		Calculator calculatorTest = new Calculator(11, tbs, mucs); 
 		calculatorTest.genPerfCount();
 		System.out.println(calculatorTest.perfCount);
+		//calculatorTest.matchFrac.get(0).set(1, 3);
+		System.out.println(calculatorTest.matchFrac);
 	}
 
 }
